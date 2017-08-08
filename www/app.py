b6e661f06034b13bcfe561fd8fa5046580389232
Administrator <yyhmmwan@gmail.com>
2017-08-08 16:40:37 +0800

    add c

--- www/app.py
+++ www/app.py
@@ -12,32 +12,12 @@
 '''
 async web application.
 '''
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
 from datetime import datetime
 
-import logging;
-
-logging.basicConfig(level=logging.INFO)
-import asyncio
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
 
-
-def index(request):
-    return web.Response(body=b'<h5>I LOVE GAKKI</h5>')
-
-
-@asyncio.coroutine
-async def init(loop):
-    app = web.Application(loop=loop)
-
-    app.router.add_route('GET', '/', index)
-
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-
-    logging.info('server started at http://127.0.0.1:9000...')
-
-    return srv
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+from config import configs
